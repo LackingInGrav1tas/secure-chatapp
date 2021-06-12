5ba17fe20b01f0bc93c968e81ba07b1f147a2e95
LackingInGrav1tas <42680395+LackingInGrav1tas@users.noreply.github.com>
2021-06-12 18:21:36 -0400

    not blue / add unencrypted tag

--- messages.py
+++ messages.py
@@ -6,6 +6,7 @@ class Message:
     def __init__(self, user, msg, encrypted=True):
         self.user = user
         self._msg = msg
+        if not encrypted: self._msg += " <unencrypted>"
         self.time = datetime.datetime.now().strftime("%x %X")
         self.encrypted = encrypted
 
@@ -26,9 +27,9 @@ class Message:
             except:
                 strmsg = '<message sent with different key>'
             if i % 2 == 0:
-                s += '<p><a' + (' style = "color: blue;"' if msg.encrypted else '') + f'>{msg.user} <textarea readonly resize: none; overflow: auto; cols="70" rows="2" style="background-color: grey; border-radius: 10px; font-family: Arial, Helvetica, sans-serif;">{strmsg}</textarea></a></p>\n'
+                s += f'<p>{msg.user} <textarea readonly resize: none; overflow: auto; cols="70" rows="2" style="background-color: grey; border-radius: 10px; font-family: Arial, Helvetica, sans-serif;">{strmsg}</textarea></p>\n'
             else:
-                s += '<p><a' + (' style = "color: blue;"' if msg.encrypted else '') + f'><textarea readonly resize: none; overflow: auto; cols="70" rows="2" style="background-color: grey; border-radius: 10px; font-family: Arial, Helvetica, sans-serif;">{strmsg}</textarea> {msg.user}</a></p>\n'
+                s += f'<p><textarea readonly resize: none; overflow: auto; cols="70" rows="2" style="background-color: grey; border-radius: 10px; font-family: Arial, Helvetica, sans-serif;">{strmsg}</textarea> {msg.user}</p>\n'
 
         return s
 
