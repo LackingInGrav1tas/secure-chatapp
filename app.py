141714012d4a3318456ef3b91de6c32b1d550515
LackingInGrav1tas <42680395+LackingInGrav1tas@users.noreply.github.com>
2021-06-15 18:14:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ function update() {
         let id = document.getElementById('convo_hash').value
         let r = await fetch('e2ee-chat-app.herokuapp.com/get_info/' + id);
         if (r.ok) {
-            let data = await r.text();
+            var data = await r.text();
             document.getElementById('txt-log').innerHTML = data;
         }
         window.alert('one interval id: ' + id + ' data: ' + data);
